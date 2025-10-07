d9d62759c904adf3da63e0a0ac25e342955a1f7b
Falcon Eagle <blue1998100@gmail.com>
2025-10-07 16:55:33 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning>
         <AuthProvider>
           <div className="relative flex min-h-screen flex-col">
             <Header />
